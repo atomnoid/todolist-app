3d55e7d74646e53d8e9d1dc69183d7dba333ea6f
atomnoid <aayushs2580@gmail.com>
2025-09-22 00:30:35 +0530

    added the frontend files

---
+++ todolist/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const __dirname = path.resolve(); // because ES modules
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
